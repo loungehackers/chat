bccf99a9dccaf242d13a04b73b800f2782024147
Stefan Wallin <stefan.w@festiz.com>
2013-10-07 00:37:42 +0200

    Better deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Take down current process
+cat tmp/pids/server.pid | xargs kill -9
+
+# Install and remove gems
 bundle install
+
+# Run migrations
 rake db:migrate
+
+# Start up new process
 bundle exec rails server -p 3000
