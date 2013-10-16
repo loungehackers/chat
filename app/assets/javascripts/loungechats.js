bc52848591f3060fd0f4640db22a021a44d744a9
Stefan Wallin <stefan.w@festiz.com>
2013-10-16 23:42:17 +0200

    Refactor javascript to be contained in the window.loungeChat object and fixed the reconnection issue.

--- app/assets/javascripts/loungechats.js
+++ app/assets/javascripts/loungechats.js
@@ -1,59 +1,51 @@
 (function() {
-	var formatMessage;
-
-	$(function() {
-		var socket;
-		var $textarea = $("form.chat textarea");
-		var $output = $("#output");
-		socket = connect();
+	window.loungeChat = {};
+	lc = window.loungeChat;
+	lc.elem = {};
+	lc.connectionTimer = "test";
+	lc.registerHandlers = function(){
+		lc = window.loungeChat;
+		socket = lc.socket;
 		socket.onopen = function(event) {
-			$output.append("<p></p>").addClass("output_login").text("Connection with chat server established.");
-		}
+			lc.elem.$textarea.prop("disabled", false);
+			var $elem = $("<p></p>").addClass("output_login").text("Connection with chat server established.");
+			lc.elem.$output.append($elem);
+			clearTimeout(lc.connectionTimer);
+		};
 		socket.onmessage = function(event) {
-			var $output, output;
 			if (event.data.length) {
-				handleMessage(event.data);
+				lc.handleMessage(event.data);
 			}
 		};
 		socket.onclose = function(event) {
-			$textarea.prop("disabled", true);
-			$output.append("<p></p>").addClass("output_logout").text("Lost connection with server, retrying connection...");
-			window.setTimeout(function(){
-				connect();
-			}, 5000);
-		}
-		$("body").on("submit", "form.chat", function(event) {
-			var $textarea;
-			event.preventDefault();
-			$textarea = $(this).find("textarea");
-			socket.send(formatMessage($textarea.val()));
-			return $textarea.val(null);
-		});
-		$textarea.focus;
-		return $textarea.on("keyup", function(event) {
-			if (event.keyCode === 13 && !event.shiftKey && $textarea.val().length != 0) {
-				return $("form.chat").submit();
-			}
-		});
-	});
-	connect = function() {
-		return new WebSocket("ws://" + window.location.host + "/chat");
-	}
-	commands = [];
-	commands["login"] = function(argument,$output) {
-		if(argument){ 
+			lc.elem.$textarea.prop("disabled", true);
+			var $elem = $("<p></p>").addClass("output_logout").text("Lost connection with server, retrying connection...");
+			lc.elem.$output.append($elem);
+			lc.connectionTimer = window.setTimeout(lc.connect, 5000);
+		};
+	};
+	lc.connect = function() {
+		lc = window.loungeChat;
+		console.info("Connecting to websocket", lc.connectionTimer);
+		lc.socket = new WebSocket("ws://" + window.location.host + "/chat");
+		lc.registerHandlers();
+		// lc.connectionTimer = window.setTimeout(lc.connect, 5000);
+	};
+	lc.commands = [];
+	lc.commands["login"] = function(argument) {
+		if(argument) {
 			console.dir(argument);
 			var divider = argument.indexOf(":");
 			var newuser = argument.slice(0,divider);
 			argument = argument.slice(divider+1,argument.length);
-			replaceUserList(argument);
+			lc.replaceUserList(argument);
 			var $elem = $("<p></p>").addClass("output_login").html("- " + newuser +" just logged in");
-			$output.append($elem);
+			window.loungeChat.elem.$output.append($elem);
 
 		}
-	}
+	};
 
-	commands["logout"] = function(argument,$output) {
+	lc.commands["logout"] = function(argument) {
 		if(argument){
 			console.log(argument);
 			var divider = argument.indexOf(":");
@@ -61,11 +53,12 @@
 			argument = argument.slice(divider+1,argument.length);
 			replaceUserList(argument);
 			var $elem = $("<p></p>").addClass("output_logout").html("- Bye bye " + olduser +"!");
-			$output.append($elem);
+			window.loungeChat.elem.$output.append($elem);
 		}
-	}
+	};
 
-	replaceUserList = function(argument) {
+	lc.replaceUserList = function(argument) {
+		/* TODO: Refactor me later, extract members into window.loungeChat */
 		var $container = $("#userlist").clone().empty();
 		var me = $container.data("me");
 		var members = $.parseJSON(argument);
@@ -77,34 +70,53 @@
 			$container.append($elem);
 		}
 		$("#userlist").replaceWith($container);
-	}
-	formatMessage = function(message) {
+	};
+	lc.formatMessage = function(message) {
 		var me;
 		me = $("#userlist").data("me");
 		return me + ": " + message;
 	};
 
-	handleMessage = function(message) {
-		if(message && message.length > 0) {
-			var $output = $("#output");
+	lc.handleMessage = function(message) {
+		if(message !== undefined && message.length > 0) {
 			console.log("message: ", message);
 			var matches = message.match(/(\[LH:)(\w+)(\])/);
-			if(matches != undefined && matches != null && matches.length > 0 && matches[1] == "[LH:") {
+			if(matches !== undefined && matches !== null && matches.length > 0 && matches[1] == "[LH:") {
 				var command = matches[2];
 				var argument = message.slice(command.length+5,message.length);
-				commands[command](argument,$output);
+				lc.commands[command](argument,window.loungeChat.elem.$output);
 			} else {
 				var me = $("#userlist").data("me");
 				message = message.replace(me, "<span class=\"output_highlight\">"+me+"</span>");
 				var $elem = $("<p></p>").addClass("output_message").html(message);
-				$output.append($elem);
+				window.loungeChat.elem.$output.append($elem);
 
 			}
-			var output = $output[0];
-			return output.scrollTop = output.scrollHeight;
+			var output = window.loungeChat.elem.$output[0];
+			output.scrollTop = output.scrollHeight;
 			
 		}
-	}
+	};
 
+
+	//INITIALIZER, let me come last please.
+	$(function() {
+		lc.elem.$textarea = $("form.chat textarea");
+		lc.elem.$output = $("#output");
+		lc.connect();
+		lc.registerHandlers();
+		$("body").on("submit", "form.chat", function(event) {
+			event.preventDefault();
+			lc.elem.$textarea = $(this).find("textarea");
+			lc.socket.send(lc.formatMessage(lc.elem.$textarea.val()));
+			return lc.elem.$textarea.val(null);
+		});
+		lc.elem.$textarea.focus();
+		return lc.elem.$textarea.on("keyup", function(event) {
+			if (event.keyCode === 13 && !event.shiftKey && lc.elem.$textarea.val().length !== 0) {
+				return $("form.chat").submit();
+			}
+		});
+	});
 
 }).call(this);
