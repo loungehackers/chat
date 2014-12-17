baf98ba310a5e4e1b7e7f451bda3e1e4e1974218
Stefan Wallin <stefan.w@festiz.com>
2014-12-17 01:22:39 +0100

    Fix production config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ LoungeChat2::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -40,7 +41,8 @@ LoungeChat2::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  #config.force_ssl = true
+  
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
